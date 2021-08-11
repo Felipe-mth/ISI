b3e94cfea3b71c3e3739982596255c0f9045f566
Felipe Matheus Costa <86928483+Felipe-mth@users.noreply.github.com>
2021-08-11 14:30:47 -0300

    Correção senha

--- js/senhas.js
+++ js/senhas.js
@@ -1,30 +1,28 @@
-var senha = document.getElementById("input-senha").value;
-var senha2 = document.getElementById("input-senha2").value;
-var nome = document.getElementById("input-nome").value;
-var user = document.getElementById("input-usuario").value;
-
+let senha = document.getElementById("input-senha").value;
+let senha2 = document.getElementById("input-senha2").value;
+let nome = document.getElementById("input-nome").value;
+let user = document.getElementById("input-usuario").value;
 function verifica_nome(){
-    if(nome == null || nome == " " || nome==""){
+    if(document.getElementById("input-nome").value == null || document.getElementById("input-nome").value == " " || document.getElementById("input-nome").value==""){
         alert("digite seu nome infeliz!!");
     }
     else{
-        alert("nome válido");
+        alert("cadastradas com sucesso nome");
     }
 }
 function verifica_usuario(){
-    if(user == null || user == " " || user==""){
+    if(document.getElementById("input-usuario").value == null || document.getElementById("input-usuario").value == " " || document.getElementById("input-usuario").value==""){
         alert("digite seu usuario infeliz!!");
     }
     else{
-        alert("usuario válido");
+        alert("cadastradas com sucesso user");
     }
 }
 function verifica_senha(){
-    if(senha != senha2 || senha == ""){
+    if(document.getElementById("input-senha").value !=  document.getElementById("input-senha2").value || document.getElementById("input-senha").value == ""){
         alert("senhas invalidas");
     }
     else{
-        
-        alert("senha aceita");
-    }
+        alert("cadastradas com sucesso senha");
+    } 
 }
